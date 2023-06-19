ecb1cf59707d8d776c43a6e90ad6227d92f0f0da
Ben Wilson <ben@registrationmax.com>
2023-06-18 22:01:42 -0400

    port

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 
 import { AppModule } from './app.module';
 
+const PORT = 42495;
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
-  await app.listen(42495, '0.0.0.0');
+  await app.listen(PORT, '0.0.0.0');
+  console.log(`http listening on port ${PORT}`)
 }
 bootstrap();
