413187f4e3a566540de4541ccd0d99e108359a94
Ben Wilson <ben@registrationmax.com>
2023-08-06 13:30:28 -0400

    killDeadClients

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -38,9 +38,6 @@ export class AppService implements OnModuleInit {
 
     @Cron(CronExpression.EVERY_10_MINUTES)
     private async killDeadClients() {
-
-        if (process.env.NODE_APP_INSTANCE == null || process.env.NODE_APP_INSTANCE == '0') {
-            await this.clientService.killDeadClients();
-        }
+        await this.clientService.killDeadClients();
     }
 }
