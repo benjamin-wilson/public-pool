c1b07ed03410e5df3583b3163d5ef61232ff11cb
Ben Wilson <ben@registrationmax.com>
2023-08-06 22:44:09 -0400

    turn off logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -38,7 +38,7 @@ const ORMModules = [
             synchronize: true,
             autoLoadEntities: true,
             cache: true,
-            logging: true,
+            logging: false,
             enableWAL: true,
             busyTimeout: 30 * 1000,
         }),
