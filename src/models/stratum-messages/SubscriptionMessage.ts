36d596bd04462e32521e35fbcbcd1eabd7838014
Ben Wilson <ben@registrationmax.com>
2023-08-09 22:28:14 -0400

    userAgent

--- src/models/stratum-messages/SubscriptionMessage.ts
+++ src/models/stratum-messages/SubscriptionMessage.ts
@@ -43,7 +43,7 @@ export class SubscriptionMessage extends StratumBaseMessage {
         // return userAgent;
         userAgent = userAgent.split(' ')[0].split('/')[0].split('V')[0];
 
-        if (userAgent.includes('bosminer')) {
+        if (userAgent.includes('bosminer') || userAgent.includes('bOS')) {
             userAgent = 'Braiins OS';
         } else if (userAgent.includes('cpuminer')) {
             userAgent = 'cpuminer';
