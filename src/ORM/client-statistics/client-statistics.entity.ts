45a107a791878b9736d59e2da292220ab0b9a81d
Ben Wilson <ben@registrationmax.com>
2023-08-11 22:35:06 -0400

    db index optimizations

--- src/ORM/client-statistics/client-statistics.entity.ts
+++ src/ORM/client-statistics/client-statistics.entity.ts
@@ -3,19 +3,19 @@ import { Column, Entity, Index, PrimaryColumn } from 'typeorm';
 import { TrackedEntity } from '../utils/TrackedEntity.entity';
 
 @Entity()
+@Index(["address", "clientName", "sessionId", "time"])
 export class ClientStatisticsEntity extends TrackedEntity {
 
     @PrimaryColumn({ length: 64, type: 'varchar' })
     submissionHash: string;
 
-    @Index()
+
     @Column({ length: 62, type: 'varchar' })
     address: string;
 
     @Column()
     clientName: string;
 
-    @Index()
     @Column({ length: 8, type: 'varchar' })
     sessionId: string;
 
