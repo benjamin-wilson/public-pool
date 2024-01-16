bbc14e22721ad18c59e7463fc3bd1ef44b5c9982
Ben Wilson <ben@registrationmax.com>
2024-01-15 20:04:26 -0500

    bug

--- src/models/StratumV1Client.ts
+++ src/models/StratumV1Client.ts
@@ -509,7 +509,7 @@ export class StratumV1Client {
                 await this.statistics.addShares(this.sessionDifficulty);
                 const now = new Date();
                 // only update every minute
-                if (now.getTime() - this.entity.updatedAt.getTime() > 1000 * 60) {
+                if (this.entity.updatedAt == null || now.getTime() - this.entity.updatedAt.getTime() > 1000 * 60) {
                     await this.clientService.heartbeat(this.entity.address, this.entity.clientName, this.entity.sessionId, this.hashRate, now);
                     this.entity.updatedAt = now;
                 }
