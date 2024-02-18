522ef126d99f8d73728d049072604800c614ad9f
Ben Wilson <ben@registrationmax.com>
2024-02-18 12:32:21 -0500

    updated at, hashrate calculation

--- src/ORM/client-statistics/client-statistics.service.ts
+++ src/ORM/client-statistics/client-statistics.service.ts
@@ -25,7 +25,8 @@ export class ClientStatisticsService {
                 shares: clientStatistic.shares,
                 acceptedCount: clientStatistic.acceptedCount,
                 updatedAt: new Date()
-            })
+            });
+
     }
     public async insert(clientStatistic: Partial<ClientStatisticsEntity>) {
         await this.clientStatisticsRepository.insert(clientStatistic);
@@ -151,14 +152,16 @@ export class ClientStatisticsService {
 
         if (result.length < 2) {
             const time = new Date(latestStat.updatedAt).getTime() - new Date(latestStat.createdAt).getTime();
-            if (time < 1) {
+            // 1min
+            if (time < 1000 * 60) {
                 return 0;
             }
             return (parseFloat(latestStat.shares) * 4294967296) / (time / 1000);
         } else {
             const secondLatestStat = result[1];
             const time = new Date(latestStat.updatedAt).getTime() - new Date(secondLatestStat.createdAt).getTime();
-            if (time < 1) {
+            // 1min
+            if (time < 1000 * 60) {
                 return 0;
             }
             return ((parseFloat(latestStat.shares) + parseFloat(secondLatestStat.shares)) * 4294967296) / (time / 1000);
